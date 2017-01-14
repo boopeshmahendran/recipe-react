87fcfd166fb7ddb4ef0a5fa7f05fb625478533f9
Boopesh <boopeshmahendran@gmail.com>
2017-01-15 00:37:37 +0530

    Added styling to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +26,7 @@ class App extends Component {
   }
   render() {
     return (
-        <div>
+        <div className='App'>
         </div>
     );
   }
