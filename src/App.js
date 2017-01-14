feb300b256ca78bcbd4e9994845ec8debc74cdd3
Boopesh <boopeshmahendran@gmail.com>
2017-01-14 20:13:05 +0530

    Created state variables in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          recipes: [
+              {
+                  name: "abcd",
+                  ingredients: "123",
+                  directions: "xyz"
+              },
+              {
+                  name: "xyz",
+                  ingredients: "234",
+                  directions: "xyz"
+              },
+              {
+                  name: "zcw",
+                  ingredients: "781",
+                  directions: "yzx"
+              },
+          ]
+      }
+  }
   render() {
     return (
         <div>
