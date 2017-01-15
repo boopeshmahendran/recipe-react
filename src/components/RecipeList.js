6b947d253ffffa264e8198e2ce17a5ee91d86df3
Boopesh <boopeshmahendran@gmail.com>
2017-01-15 12:51:07 +0530

    Added accordion to display each recipes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import Accordion from 'react-bootstrap/lib/Accordion';
 import Panel from 'react-bootstrap/lib/Panel';
 
+
 class RecipeList extends Component {
     render() {
         return (
             <Accordion>
             {this.props.recipes.map((recipe, index) => (
-                 <Recipe key={index} recipe={recipe} />
+                 <Panel key={index} header={recipe.name} eventKey={index}>
+                    <div className='ingredients'>
+                        <h4>Ingredients: </h4>
+                        <p>{recipe.ingredients}</p>
+                    </div>
+                    <div className='directions'>
+                        <h4>Directions: </h4>
+                        <p>{recipe.directions}</p>
+                    </div>
+                 </Panel>
              ))}
             </Accordion>
         );
